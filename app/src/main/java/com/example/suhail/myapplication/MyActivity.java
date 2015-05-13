efadeb880547dd4b0906d5b2be04264e1e89118a
Sultan Saadat <sultan.saadat@metikulous.com>
2015-05-13 13:50:16 +0500

    refactor

--- app/src/main/java/com/example/suhail/myapplication/MyActivity.java
+++ app/src/main/java/com/example/suhail/myapplication/MyActivity.java
@@ -5,9 +5,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -24,86 +21,51 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-
 public class MyActivity extends Activity {
-    private static final String TAG = "PostsActivity";
+    private static final String TAG = "TestApp";
 
-    //    TestAndroid test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-//        final EditText editText = (EditText) findViewById(R.id.edit_message);
-        Button btnSubmit = (Button) findViewById(R.id.btnShowMessage);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new GetServerData().execute();
-            }
-        });
-
+        new GetServerData().execute();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_my, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
     private class GetServerData extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... params) {
+
             final TestAndroid testAndroid;
+
             try {
                 String SERVER_URL = "http://bridg-dev.cloudapp.net:8080/testandroid";
-
                 HttpClient client = new DefaultHttpClient();
                 HttpGet get = new HttpGet(SERVER_URL);
-
                 HttpResponse response = client.execute(get);
                 StatusLine statusLine = response.getStatusLine();
-                System.out.println(statusLine.getStatusCode());
 
                 if (statusLine.getStatusCode() == 200) {
                     HttpEntity entity = response.getEntity();
                     InputStream content = entity.getContent();
 
                     try {
-                        //Read the server response and attempt to parse it as JSON
                         Reader reader = new InputStreamReader(content);
-
                         GsonBuilder gsonBuilder = new GsonBuilder();
                         Gson gson = gsonBuilder.create();
-
-
                         testAndroid = gson.fromJson(reader, TestAndroid.class);
-
                         content.close();
                         showInfo(testAndroid);
-
-
                     } catch (Exception ex) {
                         Log.e(TAG, "Failed to parse JSON due to: " + ex);
                     }
                 } else {
-//                    failedLoadingPosts();
+                    System.out.println("Request Failed");
                 }
             } catch (Exception ex) {
                 Log.e(TAG, "Failed to send HTTP POST request due to: " + ex);
@@ -112,7 +74,6 @@ public class MyActivity extends Activity {
         }
 
         private void showInfo(final TestAndroid test) {
-//       this.test = test;
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
