edda11cf88f9f5a0d85708723a61ec1ee2ccdcb1
suhailamir <suhail.amir101@gmail.com>
2015-05-13 16:17:03 +0500

    List view added

--- app/src/main/java/com/example/suhail/myapplication/MyActivity.java
+++ app/src/main/java/com/example/suhail/myapplication/MyActivity.java
@@ -5,6 +5,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -74,10 +76,39 @@ public class MyActivity extends Activity {
         }
 
         private void showInfo(final TestAndroid test) {
+
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    Toast.makeText(getApplicationContext(), test.getName() + " " + String.valueOf(test.getAge()) + " " + test.getSex(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Data loaded!", Toast.LENGTH_LONG).show();
+                    setContentView(R.layout.activity_my);
+                    ListView listView ;
+
+                    listView = (ListView) findViewById(R.id.list);
+                    String[] values = new String[]{test.getName(),
+                            test.getLastname(),
+                            test.getEmail(),
+                            String.valueOf(test.getAge()),
+                            test.getSex(),
+                            test.getAdd(),
+                            test.getPhone(),
+                            test.getOrg(),
+                       
+
+                    };
+//                    String[] values = new String[] { "Android List View",
+//                            "Adapter implementation",
+//                            "Simple List View In Android",
+//                            "Create List View Android",
+//                            "Android Example",
+//                            "List View Source Code",
+//                            "List View Array Adapter",
+//                            "Android Example List View"
+//                    };
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+                    listView.setAdapter(adapter);
                 }
             });
         }
--- app/src/main/java/com/example/suhail/myapplication/TestAndroid.java
+++ app/src/main/java/com/example/suhail/myapplication/TestAndroid.java
@@ -6,8 +6,53 @@ package com.example.suhail.myapplication;
 public class TestAndroid {
     private String name;
     private int age;
-    private String sex;
 
+    private String sex;
+    private String lastname;
+    private String email;
+    private String add;
+    private String org;
+    private String phone;
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
     public int getAge() {
         return age;
     }
